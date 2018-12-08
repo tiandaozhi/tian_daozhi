04f599dc6d8edfbfaefe27a2bf0dce29cfe8bd74
DESKTOP-2VOPGSP\田道智 <847156485@qq.com>
2018-12-08 18:38:26 +0800

    201701020324

--- tian_daozhi/6.35/main.cpp
+++ tian_daozhi/6.35/main.cpp
@@ -10,10 +10,9 @@ int main()
 }
 void Charge()
 {
-
-
     int a;
-    int b=1000;
+    int b=100;
+    int count=1;
     cout<<"I have a number between 1 and 1000"<<endl;
     cout<<"Can you guess my number?"<<endl;
     cout<<"Please type your first guess."<<endl;
@@ -25,18 +24,32 @@ void Charge()
     {
         cout<<"Too high.Try again"<<endl;
         cin>>a;
+        count++;
     }
 
     else if(a<b)
         {
         cout<<"Too low.Try again"<<endl;
         cin>>a;
+        count++;
     }
   }
     if(a=b)
     {
         cout<<"Excellent! You guess the number!"<<endl;
         cout<<"Would you like to play again(y or n)?"<<endl;
+        if(count<10)
+        {
+            cout<<"Either you know the secret or you got lucky!";
+        }
+       else if(count=10)
+        {
+            cout<<"Ahah! You know the secret!";
+        }
+       else  if(count>10)
+        {
+            cout<<"You should be able to do better!";
+        }
 
     }
 }
